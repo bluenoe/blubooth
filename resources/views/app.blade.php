a3c914e0889031f2d7a33a0103fbb1fb1fb986ce
bluenoe <baokhanh.dev281@gmail.com>
2025-12-24 09:22:38 +0700

    feat: redesign welcome page by replacing default Laravel content with a new layout and Lucide icons

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,8 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,600;1,400&display=swap" rel="stylesheet">
+        
         <!-- Scripts -->
         @routes
         @viteReactRefresh
